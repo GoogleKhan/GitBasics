0af01181381a93ded83b96485f547f23dfeac66c
GoogleKhan <shadab105662@gmail.com>
2022-07-26 09:20:15 +0530

    subtraction method in branch-1

--- Calculator.java
+++ Calculator.java
@@ -3,4 +3,8 @@ public class Calculator
 	public void addition(int a, int b ){
 		System.out.println(a+b);
 	}
+	public void subtraction(int a, int b ){
+		// From branch-1
+		System.out.println(a-b);
+	}
 }
